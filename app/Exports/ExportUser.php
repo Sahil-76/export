8486f259af8e033b0ae4251f865a2ebe1dba767d
Sahil Gaurav <Sahil.Gaurav@theknowledgeacademy.com>
2023-08-25 11:42:04 +0530

    commit

---
+++ app/Exports/ExportUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class ExportUser implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        // return User::select('name', 'email')->get();
+        return User::all();
+
+    }
+
+}
+
